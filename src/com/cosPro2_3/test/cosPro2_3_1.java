8bfd6908a2085bcc7e580481c88ef4ea189d261e
godwook <passionate.ksw@gmail.com>
2023-01-20 00:13:05 +0900

    commit

---
+++ src/com/cosPro2_3/test/cosPro2_3_1.java
@@ -0,0 +1,53 @@
+package com.cosPro2_3.test;
+/*
+ 작성일 : 2023.01.19
+ 작성자 : godwook
+ 문제 : CosPro2급 샘플문제 3차 1번
+ */
+import java.util.Arrays;
+
+class cosPro2_3_1 {
+    int func_a(int[] scores, int score){
+        int length = scores.length;
+        for(int rank = 0 ; rank < length ; rank++) {
+            if(scores[rank] == score)
+                return rank+1;
+
+        }
+        return 0;
+    }
+    void func_b(int[] arr){
+        Arrays.sort(arr);
+        int length = arr.length;
+        int temp = 0 ;
+        for(int i = 0 ; i<length/2;i++){
+            temp = arr[i];
+            arr[i] = arr[(length-1)-i];
+            arr[(length-1)-i] = temp;
+        }
+    }
+
+    int func_c(int[] arr, int n ){
+        return arr[n];
+    }
+
+    public int solution(int[] scores, int n){
+        int score= func_c(scores,n);
+        func_b(scores);
+        int answer = func_a(scores,score);
+        return answer;
+    }
+
+    public static void main(String[] args){
+        cosPro2_3_1 sol = new cosPro2_3_1();
+        int[] scores = {20,60,98,59};
+        int n = 3;
+
+        int ret=sol.solution(scores,n);
+
+        System.out.println(ret);
+
+    }
+
+    
+}
